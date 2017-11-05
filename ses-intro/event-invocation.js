61d32f61182c7d7e8f1cbd4ba713943d2fdf29f3
jeshan <j@jeshan.co>
2017-11-05 11:03:48 +0400

    event invocation example

---
+++ ses-intro/event-invocation.js
@@ -0,0 +1,13 @@
+
+exports.handler = (event, context, callback) => {
+  // console.log('Received event:', JSON.stringify(event, null, 2));
+  const mail = event.Records[0].ses.mail;
+
+  const {timestamp, source, messageId} = mail;
+  const receivedHeader = (mail.headers.find((x) => x.name === 'X-Received') || {}).value;
+  console.log('received', receivedHeader); // example how you can read headers
+
+  const {from, date, to, subject} = mail.commonHeaders;
+
+  context.succeed({from: from[0], to: to[0], subject, date, timestamp, source, messageId});
+};
